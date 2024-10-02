b9bca13f74c8922e838eb4a6494bad27642697be
bluka1 <luka.batarelo@gmail.com>
2024-10-02 18:15:05 +0200

    js - rad s API-jima - edit xmlHttpRequesta

--- javascript/rad-s-API-jima/script.js
+++ javascript/rad-s-API-jima/script.js
@@ -1,9 +1,16 @@
+const url = "https://dogapi.dog/api/v2/breeds";
 // var request = new XMLHttpRequest();
-// request.open("GET", "https://dogapi.dog/api/v2/breeds");
-// request.onload = TO-DO;
+// request.open("GET", url);
+
+// request.onreadystatechange = function () {
+//   if (this.readyState == 4 && this.status == 200) {
+//     console.log(JSON.parse(this.response));
+//   }
+// };
+
 // request.send();
 
-fetch("https://dogapi.dog/api/v2/breeds", { method: "GET" })
+fetch(url, { method: "GET" })
   .then((response) => response.json())
   .then((data) => console.log(data))
   .catch((err) => console.error(err));
