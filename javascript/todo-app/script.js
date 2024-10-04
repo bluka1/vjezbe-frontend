bf4f73415638c998f6581df81ebbe31a927f6085
Teo Samaržija <teo.samarzija@gmail.com>
2024-10-04 10:33:29 +0200

    js - todo app - toggleDone sada radi

--- javascript/todo-app/script.js
+++ javascript/todo-app/script.js
@@ -32,16 +32,25 @@
     showAllTodos();
   }
 
-  function toggleDone(e) {
-    console.log(e);
+  function toggleDone(id) {
+    for (let i = 0; i < todos.length; i++)
+      if (todos[i].id == id) {
+        todos[i].toggleDone();
+        break;
+      }
+    showAllTodos();
   }
 
   function showAllTodos() {
     listContainer.innerHTML = `
       ${todos
         .map(
-          (todo) => `<li class="todoItem" data-id="${todo.id}">
-          <input type="checkbox" onclick="toggleDone()" name="todoCompleted" id=${todo.id} />
+          (todo) => `<li class="todoItem ${todo.done ? "done" : ""}" data-id="${
+            todo.id
+          }">
+          <input type="checkbox" name="todoCompleted" id="checkbox_${
+            todo.id
+          }" ${todo.done ? "checked" : ""} />
           <p>${todo.text}</p>
           <button class="removeTodo">
             <svg
@@ -53,10 +62,15 @@
               <path d="M5.28 4.22a.75.75 0 0 0-1.06 1.06L6.94 8l-2.72 2.72a.75.75 0 1 0 1.06 1.06L8 9.06l2.72 2.72a.75.75 0 1 0 1.06-1.06L9.06 8l2.72-2.72a.75.75 0 0 0-1.06-1.06L8 6.94 5.28 4.22Z" />
             </svg>
           </button>
-        </li>`
+        </li>`,
         )
         .join("")}
     `;
+    todos.forEach((todo) => {
+      document.getElementById("checkbox_" + todo.id).onclick = () => {
+        toggleDone(todo.id);
+      };
+    });
   }
 
   addTodoButton.addEventListener("click", addTodoHandler);
