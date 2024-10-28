7c3d6b797975104c91ab0bb0eea13805e8219498
bluka1 <luka.batarelo@gmail.com>
2024-10-28 20:15:57 +0100

    react - prva app

---
+++ react/first-app/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <div>Moja prva aplikacija</div>;
+  }
+}
+
+export default App;
